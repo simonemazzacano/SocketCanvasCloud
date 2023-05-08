6207fb1ece4a506d1e60323405283a0b2035872e
Simone Mazzacano <84018418+s-mazza@users.noreply.github.com>
2023-05-08 22:54:48 +0200

    modifiche finali

---
+++ esSocketClientWPF/esSocketClientWPF/MainWindow.xaml.cs
@@ -0,0 +1,283 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+
+namespace esSocketClientWPF
+{
+    /// <summary>
+    /// Funzionamento generale
+    /// Gestione Canvas -->
+    /// Nell'interfaccia ci sono degli InkCanvas che hanno la proprietà Strokes di tipo StrokeCollection che contiene tutti i tratti disegnati
+    /// StrokeCollection può essere salvata su file con il formato ISF(Ink Serialized Format, dovrebbe essere base64)
+    /// Per modificare il contenuto del InkCanvas si può assegnare a Strokes un altro StrokeCollection
+    /// Quindi quello che faccio io è
+    /// Per trasformare da InkCanvas a byte[] faccio canvInvio.Strokes.Save(fs, true), poi leggo il file sotto forma di bytes (File.ReadAllBytes(inkFileName)) e i byte sono già pronti per essere inviati
+    /// Per trasformare da byte[] a InkCanvas scrivo prima su file i byte arrivati (File.WriteAllBytes(inkFileName, dati)) e poi passo il file al costruttore di StrokeCollection (StrokeCollection strokes = new StrokeCollection(fs);) e lo assegno a inkCanvas.Strokes
+    ///
+    /// Gestione socket-->
+    /// Send è un semplice socket.Send(byte della conversione canvas)
+    /// Il Receive essendo che deve essere costantemente in ascolto è un while(true) dentro un Task che legge il buffer quando c'è qualcosa
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string inkFileName = @"canvas.txt";
+        Socket socket;
+        Socket socketText;
+        bool isSending = false, isReceiving = false;
+        const int MAX_BUFFER = 1 << 15;
+        object _lock;
+        
+        public MainWindow()
+        {
+            _lock = new object();
+            InitializeComponent();
+            cp.SelectedColor = Color.FromRgb(0, 0, 0);
+        }
+
+        private byte[] CanvasToBytes()
+        {
+            try
+            {
+                using (FileStream fs = new FileStream(inkFileName, FileMode.OpenOrCreate))
+                {
+                    canvInvio.Strokes.Save(fs, true);
+                }
+
+                return File.ReadAllBytes(inkFileName);
+            }
+            catch (Exception) { }
+
+            return new byte[0];
+        }
+
+        private StrokeCollection BytesToCanvas(byte[] dati)
+        {
+            try
+            {
+                File.WriteAllBytes(inkFileName, dati);
+
+                using (FileStream fs = new FileStream(inkFileName, FileMode.Open))
+                {
+                    StrokeCollection strokes = new StrokeCollection(fs);
+                    return strokes;
+                }
+            }
+            catch (Exception) { }
+
+
+            return new StrokeCollection();
+        }
+
+        #region Gestione socket
+        private void StartServer()
+        {
+            try
+            {
+                IPHostEntry host = Dns.GetHostEntry("localhost"); //ip di host, 127.0.0.1
+                IPAddress ipHost = host.AddressList[0]; //ritorna un array ma in teoria c'è ne è solo uno
+
+                IPEndPoint valoriSocket = new IPEndPoint(ipHost, 11000); //associa ip-porta
+                IPEndPoint text = new IPEndPoint(ipHost, 10500);
+                //genera socket
+                socket = new Socket(ipHost.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                socketText = new Socket(ipHost.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                socket.Connect(valoriSocket);
+                socketText.Connect(text);
+
+                lbl_infoConnection.Content = "Socket connesso a: " + socket.RemoteEndPoint + "\t" + socketText.RemoteEndPoint;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Errore: " + ex.Message);
+            }
+        }
+
+        private async Task RiceviMessaggio()
+        {
+            string messaggio = null;
+            byte[] dati = null;
+
+            while (true)
+            {
+                while (socketText.Available <= 0) await Task.Delay(10);
+                dati = new byte[1 << 10];
+
+                int lenBytes = socketText.Receive(dati);
+                
+                messaggio += Encoding.ASCII.GetString(dati, 0, lenBytes);
+                lbl_received.Content = "Received: " + messaggio;
+                messaggio = null;
+
+            }
+
+        }
+
+        private async Task SendCanvas()
+        {
+            while (true)
+            {
+                if (isReceiving) return;
+                isSending = true;
+                byte[] messaggio = CanvasToBytes();
+                if (messaggio.Length > MAX_BUFFER) throw new Exception("Canvas supera la dimensione massima consentita");
+                int byteSent = socket.Send(messaggio);
+                isSending = false;
+                await Task.Delay(150);
+            }
+        }
+        private async Task RiceviCanvas()
+        {
+            try
+            {
+                byte[] dati;
+
+                while (true)
+                {
+                    while (isSending || socket.Available <= 0)
+                    {
+                        await Task.Delay(50);//lascia l'interfaccia fare le sue cose e poi riesegue, senza Task rimarrebbe bloccato all'infinito
+                    }
+                    dati = new byte[MAX_BUFFER];
+
+                    isReceiving = true;
+                    int lenBytes = socket.Receive(dati);
+                    byte[] toConvert = new byte[1];
+                    bool esci = false;
+
+                    //essendo che non so quanto è lungo il messaggio leggo il buffer al contrario e salto la fila di 0 alla fine, dal primo non 0 inizio a salvare i byte
+                    for (int i = lenBytes - 1; i >= 0 && !esci; i--)//un po' brutto però funziona
+                    {
+                        if (dati[i] != 0)//gli ultimi sono tutti 0
+                        {
+                            toConvert = new byte[i + 1];
+                            for (int j = 0; j <= i; j++)
+                            {
+                                toConvert[j] = dati[j];
+                            }
+
+                            esci = true;
+                        }
+                    }
+
+                    canvRicevi.Strokes = BytesToCanvas(toConvert);
+                    isReceiving = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void SendText()
+        {
+            lock (_lock)
+            {
+                Thread.Sleep(15);
+                //spedisco i dati e ricevo la risposta
+                int bytesSent = socketText.Send(Encoding.ASCII.GetBytes(txt_invia.Text));
+            }
+        }
+
+
+        #endregion
+
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                StartServer();
+                btnStart.IsEnabled = false;
+                SendCanvas();
+                RiceviMessaggio();
+                RiceviCanvas();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnInfo_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("ATTENZIONE: cliccare start sul server e in seguito sul client, l'interfaccia rimane bloccata finche non viene eseguito\n" +
+                           "Sul canvas sinistro scrivere il disegno da inviare mentre sul destro si riceve\n" +
+                           "Tramite carica file su può caricare un Canvas precedentemente salvato", "Help", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void cp_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
+        {
+            canvInvio.DefaultDrawingAttributes.Color = (Color)cp.SelectedColor;
+        }
+
+        private void btn_gomma_Click(object sender, RoutedEventArgs e)
+        {
+            canvInvio.DefaultDrawingAttributes.Color = ((SolidColorBrush)canvInvio.Background).Color;
+            cp.SelectedColor = ((SolidColorBrush)canvInvio.Background).Color;
+        }
+
+        private void txt_invia_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            SendText();
+        }
+
+        private void sld_size_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            canvInvio.DefaultDrawingAttributes.Width = sld_size.Value;
+            canvInvio.DefaultDrawingAttributes.Height = sld_size.Value;
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                lock (_lock)
+                {
+                    canvInvio.Strokes = new StrokeCollection();
+                    CanvasToBytes();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnCaricaFile_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+                ofd.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+                if (ofd.ShowDialog() == true)
+                {
+                    byte[] bytes = File.ReadAllBytes(ofd.FileName);
+                    canvInvio.Strokes = BytesToCanvas(bytes);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ esSocketWPF/esSocketWPF/GestioneDrive.xaml.cs
@@ -0,0 +1,194 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v2;
+using Google.Apis.Drive.v2.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace esSocketWPF
+{
+    /// <summary>
+    /// Logica di interazione per GestioneDrive.xaml
+    /// </summary>
+    public partial class GestioneDrive : Window
+    {
+        private DriveService service;
+        private FileList canvasCartella;
+        private string inkFileName;
+        private string idCartella;
+        public byte[] arrReturn;
+        public GestioneDrive(string inkFileName)
+        {
+            InitializeComponent();
+            this.inkFileName = inkFileName;
+            Init();
+            AggiornaLst();
+        }
+
+        private void Init()
+        {
+            string[] scopes = new string[] { DriveService.Scope.Drive,
+                                 DriveService.Scope.DriveFile};
+            string clientId = "355135089322-slkatfni9fre32j4qsqais9fbcgkapum.apps.googleusercontent.com";      // From https://console.developers.google.com
+            string clientSecret = "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";
+
+            var credential = GoogleWebAuthorizationBroker.AuthorizeAsync(new ClientSecrets
+            {
+                ClientId = clientId,
+                ClientSecret = clientSecret
+            }, scopes, Environment.UserName, CancellationToken.None, new FileDataStore("Daimto.GoogleDrive.Auth.Store")).Result;
+
+            service = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "Drive API SocketCanvas",
+            });
+
+            CreaCartella();
+        }
+        private void GetCanvasAttuali()
+        {
+            FilesResource.ListRequest req = service.Files.List();
+            req.Q = "mimeType='text/plain' and trashed=false and '" + idCartella + "' in parents";
+            canvasCartella = req.Execute();
+        }
+        private void CreaCartella()
+        {
+            FilesResource.ListRequest req = service.Files.List();
+            req.Q = "mimeType='application/vnd.google-apps.folder' and trashed=false and title contains 'SocketCanvas'";
+            FileList files = req.Execute();
+            if (files.Items.Count() == 0)
+            {
+                // Create metaData for a new Directory
+                Google.Apis.Drive.v2.Data.File bodyFold = new Google.Apis.Drive.v2.Data.File();
+                bodyFold.Title = "SocketCanvas";
+                bodyFold.Description = "Folder cotenente i disegni creati";
+                bodyFold.MimeType = "application/vnd.google-apps.folder";
+
+                try
+                {
+                    FilesResource.InsertRequest request = service.Files.Insert(bodyFold);
+                    Google.Apis.Drive.v2.Data.File _ = request.Execute();
+                    files = req.Execute();
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Errore: " + e.Message, "Errore", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            idCartella = files.Items[0].Id;
+        }
+
+        private void UploadFile()
+        {
+            Google.Apis.Drive.v2.Data.File body = new Google.Apis.Drive.v2.Data.File();
+            string fileName =  chkPersonalizza.IsChecked == false || string.IsNullOrEmpty(txtNomeCanvas.Text) ? inkFileName : txtNomeCanvas.Text;
+            body.Title = fileName + "-" + DateTime.Now.ToString("s");
+            body.Description = "File uploaded by Socket Canvas";
+            body.Parents = new List<ParentReference>() { new ParentReference() { Id = idCartella } };
+
+            byte[] byteArray = System.IO.File.ReadAllBytes(inkFileName);
+            System.IO.MemoryStream stream = new System.IO.MemoryStream(byteArray);
+
+            FilesResource.InsertMediaUpload request = service.Files.Insert(body, stream, "text/plain");
+            request.Upload();
+
+            AggiornaLst();
+        }
+
+        private void AggiornaLst()
+        {
+            GetCanvasAttuali();
+
+            FilesResource.ListRequest req = service.Files.List();
+            req.Q = "mimeType='text/plain' and trashed=false and '" + idCartella + "' in parents";
+            canvasCartella = req.Execute();
+            lstCaricati.Items.Clear();
+            foreach (var item in canvasCartella.Items)
+            {
+                lstCaricati.Items.Add(item.Title);
+            }
+        }
+
+        private void MostraCanvasSelezionato()
+        {
+            GetCanvasAttuali();
+            int idxSelezionato = lstCaricati.SelectedIndex;
+            if(idxSelezionato == -1) throw new Exception("nessun canvas selezionato");
+            var selected = canvasCartella.Items[idxSelezionato];
+
+            var x = service.HttpClient.GetByteArrayAsync(selected.DownloadUrl);
+            byte[] arrBytes = x.Result;
+
+            arrReturn = arrBytes;
+        }
+
+        private void w_Chiusura(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            this.Hide();
+        }
+
+        private void btnUsaSelezionato_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //mostra message box con messaggio di conferma e se si clicca ok allora esegue il codice
+                MessageBoxResult result = MessageBox.Show("Il canvas attuale verrà sovrascritto da quello selezionato, continuare? Verra visualizzato alla" +
+                    "chiusura della pagina", "Conferma", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (result == MessageBoxResult.Yes)
+                {
+                    MostraCanvasSelezionato();
+                }
+            }catch (Exception ex)
+            {
+                MessageBox.Show("Errore: " + ex.Message, "Errore", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void btnCaricaCanvas_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                UploadFile();
+                AggiornaLst();
+            }catch (Exception ex)
+            {
+                MessageBox.Show("Errore: " + ex.Message, "Errore", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        //se chkPersonalizza è checckato allora abilita txtNomeCanvas
+        private void chkPersonalizza_Checked(object sender, RoutedEventArgs e)
+        {
+            txtNomeCanvas.IsEnabled = true;
+        }
+
+        //se chkPersonalizza non è checckato allora disabilita txtNomeCanvas
+        private void chkPersonalizza_Unchecked(object sender, RoutedEventArgs e)
+        {
+            txtNomeCanvas.IsEnabled = false;
+        }
+
+        private void btnInfo_Click(object sender, RoutedEventArgs e)
+        {
+            //message box per spiegare il funzionamento dell'interfaccia
+            MessageBox.Show("Usa selezionato carica nel canvas invio il canvas selezionato dal list box\n" +
+                "Carica attuale carica nella cartella drive il canvas invio corrente, con opzione di rinominarlo a piacere spuntando 'Personalizza nome'", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
